839f1952a02c638c76dc7be66ec5065a62fd2688
Rodrigo Farid <rodrigolopezcordoba805@gmail.com>
2026-03-20 07:50:22 -0600

    Análisis

--- src/main/java/mx/edu/prounidad2/nlp/implementacion/ia/AnalisisEstadistico.java
+++ src/main/java/mx/edu/prounidad2/nlp/implementacion/ia/AnalisisEstadistico.java
@@ -1,4 +1,8 @@
 package mx.edu.prounidad2.nlp.implementacion.ia;
 
 public class AnalisisEstadistico {
+
+    public static void main(String[] args) {
+        System.out.println("Análisis estadístico");
+    }
 }
